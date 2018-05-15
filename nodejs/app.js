aae2b9f39fb8eca4551990806870f8319052850c
unknown <dlwjdaud1@gmail.com>
2018-05-15 21:06:24 +0900

    app.js modified

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var mysql = require('mysql');
 var cron = require('node-cron');
-
+var json2csv = require('json2csv');
 var app = express();
 var port = 3000;
 
@@ -36,8 +36,7 @@ app.get('/update',(req, res)=>{
             console.log(res);
         });    
     }
-    insert_db(query);
-    
+    insert_db(query);   
     res.send(query);
 });
 
@@ -61,6 +60,60 @@ cron.schedule('0,10,20,30,40,50 * * * * *', function(req,res){
     });
 });
 */
+
+app.get('/download', (req,res) => {
+    console.log('download csv file');
+    var tab_name = req.query.tab_name;
+
+    var sql =  'select * from '+ tab_name + ' order by id asc';
+    connection.query(sql, function(err,row){
+        if(err){
+            throw err;
+            return;
+        }
+        if(tab_name == 'indoor')
+            res.attachment('indoor.csv');
+        else if(tab_name == 'outdoor');
+            res.attachment('outdoor.csv');
+        var output = json2csv.parse(row);
+        res.status(200).send(output);
+    });
+});
+
+app.get('/read', (req,res) => {
+    var sql = 'select * from indoor order by id desc limit 1';
+    var indoor={};
+    var outdoor={};
+    connection.query(sql,function(err,row,col){
+        if(err){
+            throw err;
+            return;
+        }
+        indoor = row[0];
+        console.log(indoor);
+    });
+    sql = 'select * from outdoor order by id desc limit 1';
+    connection.query(sql,function(err,row,col){
+        if(err){
+            throw err;
+            return;
+        }
+        outdoor = row[0];
+        console.log(outdoor);
+    });
+
+    console.log(indoor.temperature, outdoor.temperature);
+
+    
+    if(indoor.temperature > outdoor.temperature+2){
+        res.send('0');
+        console.log('0');
+    }
+    else {
+        res.send('1');
+        console.log('1');
+    }
+});
 
 app.listen(port,()=>{
     console.log('listening to port ' + port);
