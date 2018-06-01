1bc10339734354cc5ff6aa8f1a1718ccca08f5f3
fast-squid <dlwjdaud1@gmail.com>
2018-06-01 20:53:22 +0900

    update 06-01

--- nodejs/app.js
+++ nodejs/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var mysql = require('mysql');
 var fs = require('fs');
 var json2csv = require('json2csv');
+var http = require('http');
 var app = express();
 var port = 3000;
 
@@ -85,7 +86,7 @@ app.get('/view', (req,res)=>{ // not for access
             dust_pm10_sum += row[i].dust_pm10;
         }
         res.append('Data', JSON.stringify(json));
-        res.status(200).send()
+        res.status(200).send();
     });  
 });
 
@@ -125,45 +126,115 @@ app.get('/download', (req,res) => {
     });
 });
 
+app.get('/write', (req,res)=>{
+    console.log(req.query);
+    fs.writeFileSync('manual.txt',req.query.manual);
+    res.status(200).send();
+});
+
+function cal_pollution(table, discomfort){
+    var val = new Array();
+    val[0] = table.dust_pm10;
+    val[1] = table.dust_pm25;
+    val[2] = discomfort;
+    val[3] = table.co2;
+
+    console.log(val); 
+    var std_low = [[0,31,81,151],[0,16,36,76],[0,68,75,80],[0,451,1001,2001]];
+    var std_high = [[30,80,150,600],[15,35,75,500],[67,74,79,100],[450,1000,2000,5000]];
+
+    var score_low=[0,51,101,251 ]; 
+    var score_high=[50,100,250,500 ];
+    var score=0;
+    var max=0;
+    var field_count=0;
+    
+    for(var i=0; i<4; i++){
+        for(var j=0; j<4; j++){
+            if(std_low[i][j]<=val[i]&&val[i]<=std_high[i][j]){
+                score=(score_high[j]-score_low[j])/(std_high[i][j]-std_low[i][j])*(val[i]-std_low[i][j])+score_low[j];
+                if(max<score)
+                    max=score;
+                if(j>1)
+                    field_count++;                        
+            }
+        }
+    }
+
+    if(field_count==2)
+        return max+50;
+    else if(field_count>2)
+        return max+75;
+    else
+        return max;
+}
+
+function calc_discomfort(table){
+    var temperature = table.temperature;
+    var humidity = table.humidity;
+    return 9/5*temperature-0.55*(1-humidity/100)*(9/5*temperature-26)+32;
+}
+
 app.get('/read', (req,res) => {
     var indoor={};
     var outdoor={};
+    var manual;
+    manual = fs.readFileSync('manual.txt','utf-8');
+    console.log(manual);
 
-    var in_promise = new Promise(function (resolve, reject) {
-        var sql = 'select * from indoor order by id desc limit 1';
-        connection.query(sql,function(err,row,col){
-            if(err){
-                throw err;
-                return;
-            }
-            indoor = row[0];
-            resolve();
+    if(manual == 2){
+        var in_promise = new Promise(function (resolve, reject) {
+            var sql = 'select * from indoor order by id desc limit 1';
+            connection.query(sql,function(err,row,col){
+                if(err){
+                    throw err;
+                    return;
+                }
+                indoor = row[0];
+                resolve();
+            });
         });
-    });
 
-    var out_promise = new Promise(function (resolve, reject) {
-        sql = 'select * from outdoor order by id desc limit 1';
-        connection.query(sql,function(err,row,col){
-            if(err){
-                throw err;
-                return;
-            }
-            outdoor = row[0];
-            resolve();
+        var out_promise = new Promise(function (resolve, reject) {
+            sql = 'select * from outdoor order by id desc limit 1';
+            connection.query(sql,function(err,row,col){
+                if(err){
+                    throw err;
+                    return;
+                }
+                outdoor = row[0];
+                resolve();
+            });
         });
-    });
 
-    Promise.all([in_promise, out_promise]).then(function (text){
-        if(indoor.temperature > 27)
-            res.send('0');
-        else 
-            res.send('1');
-    }, function (error) {
+        Promise.all([in_promise, out_promise]).then(function (text){
+            var discomfort;
+            var indoor_pol;
+            var outdoor_pol;
 
-    });
+            discomfort = calc_discomfort(indoor);
+            indoor_pol = cal_pollution(indoor,discomfort);
+            discomfort = calc_discomfort(outdoor);
+            outdoor_pol = cal_pollution(outdoor,discomfort);
 
+            console.log(indoor_pol);
+            console.log(outdoor_pol);
+            if(indoor_pol > outdoor_pol - 10)
+                res.send('0');
+            else 
+                res.send('1');
+        }, function (error) {
+
+        });
     }
-
+    else if(manual == 1){
+        console.log("manual on");
+        res.send('0');
+    }
+    else{
+        console.log("manual off");
+        res.send('1');
+    }
 });
 
 
